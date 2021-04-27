392b5e0561bad6733ef55a33c1e56964ea5da27d
Bipin <bipp2009@gmail.com>
2021-04-27 18:33:43 +0100

    Added description and got content string being created from generate markdown function

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require('util');
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -8,15 +14,24 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?'
+    },
+    {
+        tyle: 'input',
+        name: "description",
+        message: "Please describe your project"
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    console.log(data)
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(response => console.log(response))
+    inquirer.prompt(questions).then((response) => 
+        writeToFile('readme.md', generateMarkdown(response) )
+        )
 }
 
 // Function call to initialize app
